9cd34a78a9a42afc513730676cf0e68156f52d7e
Ashvini379 <44570192+Ashvini379@users.noreply.github.com>
2024-03-30 10:54:14 +0530

    Update While Loop.sh

--- Day 5  Mastering Bash Scripting/While Loop.sh
+++ Day 5  Mastering Bash Scripting/While Loop.sh
@@ -6,3 +6,19 @@ do
  echo "Numbers are $count"
  let count++
 done
+
+
+#while loop to read content from a file
+
+while read myVar
+do
+ echo "Value from file $myVar"
+done < names.txt
+
+#while loop to read content from csv file
+while IFS="," read id name age
+do
+ echo "Id is $id "
+ echo "name is $name"
+ echo "age is $age"
+done < names.csv
