ca6118dcebd5f336ea7c40c8a69c528c720df3ed
Ashvini379 <44570192+Ashvini379@users.noreply.github.com>
2024-03-30 10:49:11 +0530

    Create CaseConversion.sh
    Case Conversion

---
+++ Day 5  Mastering Bash Scripting/CaseConversion.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+text="Hello World"
+lowercase="${text,,}"  #Converts to "hello world"
+echo "Lower case is $lowercase"
+
+uppercase="${text,,}"  #Converts to "hello world"
+echo "Uppper case is $uppercase"
