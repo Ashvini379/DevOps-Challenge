5a6051d0f8d4037bed6b95a5096e40fd964a2510
Ashvini379 <44570192+Ashvini379@users.noreply.github.com>
2024-03-30 10:44:40 +0530

    Create Variables.sh
    Veriables in Bash

---
+++ Day 5  Mastering Bash Scripting/Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Assign values to variable directly
+city="Pune"
+echo "city name is $city"
+
+#!/bin/bash
+#Assign value based on output from another command
+HOSTNAME=$(hostname)
+echo "Hostname is $HOSTNAME"
